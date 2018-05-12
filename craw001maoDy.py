cef0b4bbd558ce5644c17989177888b5802a95ba
linhai <415787837@qq.com>
2018-05-12 17:11:51 +0800

    更新001mao电影类

--- craw001maoDy.py
+++ craw001maoDy.py
@@ -1,9 +1,7 @@
-# https://jzav-cloudflare.club/share/39Y8Yj0kv46wGAbw
-
 # -*- coding: utf-8-*-
 __author__ = 'lihailin'
 __email__ = '1501210931@qq.com'
-__date__ = '2018年5月10日'
+__date__ = '2018年5月12日'
 
 import os
 import time
@@ -23,42 +21,64 @@ class Craw001maoDy(craw001maoPic.Craw001maoPic):
     '''
     def __init__(self):
         super(Craw001maoDy, self).__init__()
-        self.xs001maoInfo = mongoDb.MongoDbOpreater('crawPorn', '001maoDy')
+        self.dy001maoInfo = mongoDb.MongoDbOpreater('crawPorn', '001maoDy')
         self.num = 1
         # self.startUri = super(Craw001maoXs, self).startUri
         # self.baseUri = super()
 
-    def saveXs(self, url):
+    def insertVideoDb(self, videoUris, videoPics, videoDescs, classon):
         '''
-        保存小说
+        保存视频信息
+        :return:
+        '''
+        for i, c in enumerate(videoUris):
+
+            rd = {}
+            rd['id'] = self.num
+            rd['class'] = classon
+            rd['desc'] = videoDescs[i]
+            rd['videoUrl'] = c
+            rd['videoFengmian'] = videoPics[i]
+            insertTime =  time.strftime('%Y/%m/%d %H:%M:%S',time.localtime(time.time()))
+            rd['insertTime'] = insertTime
+            # print(rd)
+            self.num += 1
+            if self.dy001maoInfo.insert(rd):
+                logg.info('success insert into mongo xs: %s' % c)
+            else:
+                logg.info('fail insert into mongo xs: %s' % c)
+
+    def getVideoInfo(self, url):
+        '''
+        保存电影信息
+        :param url:
         :return:
         '''
         r = requests.get(url, timeout=5).text  # 不要代理
-        rd = {}
-        rd['id'] = self.num
-        sel = etree.HTML(r)
-        xsDesc = sel.xpath('//span[@class="cat_pos_l"]/a')
-        xsDesc = os.path.join(self.picDictory, xsDesc[1].text, xsDesc[2].text)
-        rd['desc'] = xsDesc.split('/')[-1]
-        rd['url'] = url
-        rd['localPath'] = xsDesc
-        insertTime =  time.strftime('%Y/%m/%d %H:%M:%S',time.localtime(time.time()))
-        rd['insertTime'] = insertTime
-        rd['content'] = r
-        if self.pic001maoInfo.insert(rd):
-            logg.info('success insert into mongo xs: %s' % url)
-        else:
-            logg.info('fail insert into mongo xs: %s' % url)
 
+        sel = etree.HTML(r)
+        videoUris = sel.xpath('//div[@class="box dy_list"]/ul/li/a/@href')
+        videoUris = list(map(lambda x:self.baseUri+x , videoUris))
+        # print(xsDescs)
+        videoPics = sel.xpath('//div[@class="box dy_list"]/ul/li/a/img/@src')
+        videoDescs = sel.xpath('//div[@class="box dy_list"]/ul/li/a/h3')
+        videoDescs = list(map(lambda x:x.text , videoDescs))
+        classon = sel.xpath('//span[@class="cat_pos_l"]/a')[1].text
+        # print(videoUris)
+        # print(videoPics)
+        # print(videoDescs)
+        self.insertVideoDb(videoUris, videoPics, videoDescs, classon)
 
     def engin(self):
-        # classUris = self.getFirstPage(self.startUri)
-        # for classUri in classUris[8:16]:  # 得到分类,前8个为图片
-        #     print(classUri)
+        classUris = self.getFirstPage(self.startUri)
+        for classUri in classUris[16:24]:  # 得到分类,前8个为图片
+            cls = self.classUriGen(classUri)
+            for cl in cls:
+                self.getVideoInfo(cl)
+                print(cl)
         #     xsUris = self.getSecondPagePic(classUri)
         #     for xsUri in xsUris:
         #         self.saveXs(xsUri)
-        self.logCrawPic('https://jzav-cloudflare.club/share/39Y8Yj0kv46wGAbw', {}, 't.mp4')
 
 if __name__ == '__main__':
     craw001maoXs = Craw001maoDy()
